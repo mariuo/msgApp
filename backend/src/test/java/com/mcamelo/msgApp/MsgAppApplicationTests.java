aa73a7f3ffef04a9076c5426d2384f5aca8bf4b4
Mario Camelo <mariuo@gmail.com>
2022-07-30 12:46:35 -0400

    Project Created

---
+++ backend/src/test/java/com/mcamelo/msgApp/MsgAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcamelo.msgApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsgAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
